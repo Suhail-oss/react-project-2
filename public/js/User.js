081d500c89ac78cf4f33e6bed1aa4b5a7699e3db
Suhail-oss <135872832+Suhail-oss@users.noreply.github.com>
2024-03-16 12:57:55 +0530

    upload

---
+++ public/js/User.js
@@ -0,0 +1,14 @@
+
+
+import React,{ Component} from 'react'
+
+export default class User extends Component {
+    
+
+    render(){
+        console.log("User render");
+        return(
+           <div>User</div>
+        )
+    }
+}
---
+++ src/function/Head.js
@@ -0,0 +1,12 @@
+
+import "./style.css"
+function  Head(){
+    return(
+        <div className="head">
+            <h2 >MY-New-Static-Website</h2>
+            <img src="logo/slogo.jpg" height="100px" width="100px" />
+
+        </div>
+    )
+}
+export default Head;
